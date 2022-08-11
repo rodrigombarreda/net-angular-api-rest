f048c2c6660de12ff408e63607401096604fa9ae
Barreda <rodrigo.barreda@accusys.com.ar>
2022-08-11 09:25:49 -0300

    commitv3

--- TarjetaCreditoFront/FETarjetaCredito/src/app/app-routing/app-routing.module.ts
+++ TarjetaCreditoFront/FETarjetaCredito/src/app/app-routing/app-routing.module.ts
@@ -6,6 +6,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 
 const routes:Routes=[
+  { path: '', component: LoginComponent},
   {path: 'login',component:LoginComponent},
   {path: 'register',component:RegisterComponent}
   ];
